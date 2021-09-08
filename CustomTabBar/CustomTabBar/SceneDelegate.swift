b2198f8a6f7d6ac90df3ce867b45632be3021f53
Ramsey <seamusapple@163.com>
2021-09-08 23:14:44 +0800

    add constraint for custom root tab bar controller

--- CustomTabBar/CustomTabBar/SceneDelegate.swift
+++ CustomTabBar/CustomTabBar/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let rootViewController = ViewController()
+        let rootViewController = RootStackTabViewController()
         window.rootViewController = rootViewController
         self.window = window
         window.makeKeyAndVisible()
--- CustomTabBar/CustomTabBar/TabBarController/CustomTabBarController/RootStackTabViewController.swift
+++ CustomTabBar/CustomTabBar/TabBarController/CustomTabBarController/RootStackTabViewController.swift
@@ -8,14 +8,67 @@
 import UIKit
 
 class RootStackTabViewController: UIViewController {
-
+    // MARK: - Public Methods
+    
+    // MARK: - Public Properties
     var currentIndex = 0
     
-    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        initSubComponents()
+        addSubComponents()
+        layoutSubComponents()
     }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Init Methods
+    
+    // MARK: - Super Override
+    
+    // MARK: - Private Methods
+    private func initSubComponents() {
+        tabBackView.translatesAutoresizingMaskIntoConstraints = false
+        tabBackView.backgroundColor = .green
+        tabContainerView.translatesAutoresizingMaskIntoConstraints = false
+        tabContainerView.backgroundColor = .gray
+        tabContainerView.distribution = .equalSpacing
+        tabContainerView.alignment = .fill
+        tabContainerView.spacing = 0
+        tabContainerView.axis = .horizontal
+    }
+    
+    private func addSubComponents() {
+        view.addSubview(tabBackView)
+        tabBackView.addSubview(tabContainerView)
+    }
+    
+    private func layoutSubComponents() {
+        view.addConstraints([
+            NSLayoutConstraint(item: tabBackView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: tabBackView, attribute: .right, relatedBy: .equal, toItem: view, attribute: .right, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: tabBackView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: tabBackView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 0.12, constant: 0)
+        ])
+        tabBackView.addConstraints([
+            NSLayoutConstraint(item: tabContainerView, attribute: .left, relatedBy: .equal, toItem: tabBackView, attribute: .left, multiplier: 1, constant: 15),
+            NSLayoutConstraint(item: tabContainerView, attribute: .right, relatedBy: .equal, toItem: tabBackView, attribute: .right, multiplier: 1, constant: -15),
+            NSLayoutConstraint(item: tabContainerView, attribute: .top, relatedBy: .equal, toItem: tabBackView, attribute: .top, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: tabContainerView, attribute: .bottom, relatedBy: .equal, toItem: tabBackView, attribute: .bottom, multiplier: 1, constant: 0)
+            
+        ])
+    }
+    
+    // MARK: - Event Responses
+    
+    // MARK: - Private Methods
+    
+    // MARK: - Private Properties
+    private let tabBackView = UIView()
+    private let tabContainerView = UIStackView()
 }
