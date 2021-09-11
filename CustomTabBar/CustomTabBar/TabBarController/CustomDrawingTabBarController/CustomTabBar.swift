9e1fc12f065573686c0ce7d47baf3350cf926e5d
Ramsey <seamusapple@163.com>
2021-09-11 23:19:11 +0800

    Add a tricky shape layer for tabBar

--- CustomTabBar/CustomTabBar/TabBarController/CustomDrawingTabBarController/CustomTabBar.swift
+++ CustomTabBar/CustomTabBar/TabBarController/CustomDrawingTabBarController/CustomTabBar.swift
@@ -37,17 +37,37 @@ class CustomTabBar: UITabBar {
         self.tintColor = UIColor(named: "DeepSaffron")
     }
     
+    fileprivate func addAnotherTrickyQuadCurve(_ path: UIBezierPath, _ centerWidth: CGFloat, _ height: CGFloat) {
+        path.move(to: CGPoint(x: 0, y: -20)) // Start Point
+        path.addLine(to: CGPoint(x: centerWidth-50, y: 0)) // Left slope
+        path.addQuadCurve(to: CGPoint(x: centerWidth-30, y:20), controlPoint: CGPoint(x: centerWidth-30, y: 5)) // Top left curve
+        path.addLine(to: CGPoint(x: centerWidth-29, y: height-10)) // Left vertical line
+        path.addQuadCurve(to: CGPoint(x: centerWidth, y:height+10), controlPoint: CGPoint(x: centerWidth-30, y: height+10)) // Bottom left curve
+        path.addQuadCurve(to: CGPoint(x: centerWidth+40, y:height-10), controlPoint: CGPoint(x: centerWidth+40, y: height+10)) // Bottom right curve
+        path.addLine(to: CGPoint(x: centerWidth+41, y: 20)) // Right vertical line
+        path.addQuadCurve(to: CGPoint(x: centerWidth+50, y:0), controlPoint: CGPoint(x: centerWidth+41, y: 5)) // Top right curve
+        path.addLine(to: CGPoint(x: self.frame.width, y: -20)) // Right slope
+        path.addLine(to: CGPoint(x: self.frame.width, y: self.frame.height))
+        path.addLine(to: CGPoint(x: 0, y: self.frame.height))
+        path.close()
+    }
+    
     private func createPath() -> CGPath {
         let height: CGFloat = 15
         let path = UIBezierPath()
         let centerWidth = self.frame.width/2
         
+        addQuadCurvePathOne(path, centerWidth, height)
+//        addAnotherTrickyQuadCurve(path, centerWidth, height)
+        
+        return path.cgPath
+    }
+    
+    fileprivate func addQuadCurvePathOne(_ path: UIBezierPath, _ centerWidth: CGFloat, _ height: CGFloat) {
         path.move(to: CGPoint(x: 0, y: 0))
         path.addQuadCurve(to: CGPoint(x: self.frame.width, y: 0), controlPoint: CGPoint(x: centerWidth, y: height))
         path.addLine(to: CGPoint(x: self.frame.width, y: self.frame.height))
         path.addLine(to: CGPoint(x: 0, y: self.frame.height))
         path.close()
-        
-        return path.cgPath
     }
 }
